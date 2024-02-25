01967c3df3645c3335f05475d77379376c01127c
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2024-02-25 11:04:02 -0600

    Update delete-cluster.sh

--- install/script/delete-cluster.sh
+++ install/script/delete-cluster.sh
@@ -2,7 +2,7 @@
 rosa delete cluster --cluster=<clustername> --watch
 
 # Delete operator role
-rosa delete operator-roles --prefix <role perefix>
+rosa delete operator-roles --prefix <role perefix> --mode auto -y
 
 # Delete OIDC config
-rosa delete oidc-provider --oidc-config-id <ID>
+rosa delete oidc-provider --oidc-config-id <ID> --mode auto -y
