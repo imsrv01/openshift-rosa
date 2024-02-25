b4b0d54c5bf6b3ffbda3675bffb4a473c45b40d6
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2024-02-25 11:36:58 -0600

    Update create-cluster-public.sh

--- install/script/create-cluster-public.sh
+++ install/script/create-cluster-public.sh
@@ -22,7 +22,7 @@ OPERATOR_ROLES_PREFIX=<prefix_name>
 rosa create operator-roles --hosted-cp --prefix $OPERATOR_ROLES_PREFIX --oidc-config-id $OIDC_ID --installer-role-arn arn:aws:iam::${AWS_ACCOUNT_ID}:role/${ACCOUNT_ROLES_PREFIX}-HCP-ROSA-Installer-Role
 
 # Create cluster
-rosa create cluster --cluster-name=<cluster_name> --sts --mode=auto --hosted-cp --operator-roles-prefix <operator-role-prefix> --oidc-config-id <ID-of-OIDC-configuration> --subnet-ids=<public-subnet-id>,<private-subnet-id>
+rosa create cluster --cluster-name=<cluster_name> --sts --mode=auto --hosted-cp --oidc-config-id <ID-of-OIDC-configuration> --subnet-ids=<public-subnet-id>,<private-subnet-id> -y
 
 # describe cluster
 rosa describe cluster -c rosalab5
